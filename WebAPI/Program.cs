f7fabdd52da8dd581401fc9365050e7238723cd9
Luna Goodnight <luna.lee@nextfortune.tw>
2022-03-16 17:38:40 +0800

    [Add] enable cors

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,20 @@
+
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
+// https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-6.0#attr
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyPolicy",
+        builder =>
+        {
+            builder.WithOrigins("https://luna.pinkjelly.org/",
+                    "http://localhost:3000",
+                    "http://localhost:3001"
+                    )
+                .WithMethods("PUT", "DELETE", "GET");
+        });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -10,14 +25,23 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
 
 app.UseHttpsRedirection();
 
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
